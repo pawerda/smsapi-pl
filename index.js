40d7e4518a4edb35e6496316c03252386c133a57
pawel.werda@websensa.com <pawel.werda@websensa.com>
2015-06-12 15:35:46 +0200

    Version 0.1.0 soft reset

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-
-
+'use strict';
 exports.Message = require('./lib/message.js');
 exports.API = require('./lib/config.js');
+exports.setSender = function(sender_config){
+     return exports.sender = new exports.API(sender_config);
+};
